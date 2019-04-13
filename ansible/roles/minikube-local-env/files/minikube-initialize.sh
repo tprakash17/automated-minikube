9f84f72f2eb0e43e8ced201d0bd582f51c4152ea
Tarun Prakash <tarun.prakash17@gmail.com>
2019-04-13 17:23:38 +0530

    Vagrant setup for minikube

---
+++ ansible/roles/minikube-local-env/files/minikube-initialize.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export MINIKUBE_WANTUPDATENOTIFICATION=false
+export MINIKUBE_WANTREPORTERRORPROMPT=false
+export MINIKUBE_HOME=$HOME
+export CHANGE_MINIKUBE_NONE_USER=true
+
+mkdir -p $HOME/.kube
+touch $HOME/.kube/config
+export KUBECONFIG=$HOME/.kube/config
+
+
+# sudo -E /usr/local/bin/minikube start --vm-driver=none --kubernetes-version=v1.11.0
